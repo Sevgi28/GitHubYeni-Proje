6b9ec0db26ba420da19f8d736d152da0164242a7
Sevgi28 <sevgihakan1428@gmail.com>
2022-12-24 23:37:26 +0100

    Sevginin ilk commiti

---
+++ C02.java
@@ -0,0 +1,7 @@
+public class C02 {
+
+    public static void main(String[] args) {
+        System.out.println("git status versiyon durumuzu bize soyler");
+        System.out.println("Cmd cok kullanisli");
+    }
+}
